50e47a4ddd8adc0741d2e73d8d17e2c019e9aeac
Abolo Samuel <ikabolo59@gmail.com>
2021-07-30 03:25:24 +0100

    Patterns with c++

---
+++ patterns.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int loop, n = 5;
+
+    cout<<"Which of the patterns do you want. 1,2,3 or 4: ";
+    cin>>loop;
+
+
+    switch (loop) {
+        case 1:
+            for ( int i = 0; i < n; i++ ) {
+                for ( int j = 0; j <= i; j++ ) {
+                    cout<<"*";
+                }
+                cout<<endl;
+            }
+            break;
+        case 2:
+            for ( int i = 0; i < n; i++ ) {
+                for ( int j = 0; j < n; j++ ) {
+                    cout<<"*";
+                }
+                cout<<endl;
+            }
+            break;
+        case 3:
+            for ( int i = 0; i < n; i++ ) {
+                cout<<"*\n";
+            }
+            break;
+        case 4:
+            for ( int i = n; i > 0; i-- ) {
+                for ( int j = i; j > 0; j-- ) {
+                    cout<<"*";
+                }
+                cout<<endl;
+            }
+            break;
+    }
+
+    return 0;
+}
